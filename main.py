2a3f8a42c2171b16f817864a77882fa69e4aff85
mono-1729 <kawai.172957@gmail.com>
2023-02-07 21:53:34 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import whisper
+model = whisper.load_model("base")
+# result = model.transcribe("準備したファイル名を指定") # 今回の記事ではtest.m4aを用います。
+result = model.transcribe("CZLLVP-Qd5c.mp3", verbose=True)
+print(result["text"])
