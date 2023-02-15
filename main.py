e58c27cda0ace5ed0b59604e30f15c1f86eca952
mono-1729 <kawai.172957@gmail.com>
2023-02-15 15:35:14 +0900

    ディレクトリ整理

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import whisper
-model = whisper.load_model("base")
+from stt import load_model
+model = load_model("base")
 # result = model.transcribe("準備したファイル名を指定") # 今回の記事ではtest.m4aを用います。
 result = model.transcribe("CZLLVP-Qd5c.mp3", verbose=True,language="ja",)
 sentences=list(result['text'].split("\n"))
